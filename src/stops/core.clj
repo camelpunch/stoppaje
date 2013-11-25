50b4550cbb68c222bd86b6a14b1d7adcc24aeb95
Andrew Bruce <andrew@camelpunch.com>
2013-11-24 19:08:58 -0800

    pretty printing

--- src/stops/core.clj
+++ src/stops/core.clj
@@ -12,36 +12,38 @@
 ; (defn prediction-xml [] (slurp "prediction.xml"))
 (defn route-xml [] (slurp "43.xml"))
 
-(defn first-content [xml]
-  (-> xml parse-str :content first))
+(defn body [xml] (-> xml parse-str :content first))
 
 (defn predictions [xml]
-  (let [raw-predictions (map :attrs (:content (first (:content (first-content xml)))))]
+  (let [raw-predictions (map :attrs (:content (first (:content (body xml)))))]
     (map (fn [prediction]
            {:arrival-datetime (to-time-zone (from-long (read-string (prediction :epochTime))) (default-time-zone))
             :arrival-minutes (prediction :minutes)}
            ) raw-predictions)))
 (predictions (prediction-xml "5171"))
+(defn pretty-prediction [stop-tag]
+  (let [all-predictions (predictions (prediction-xml "5171"))]
+    (str "Arriving in " (:arrival-minutes (first all-predictions)) " minutes")))
+(pretty-prediction "5171")
 
-; (defn title [route]
-;   (-> route :attrs :title))
+(defn route-title [xml] (-> xml body :attrs :title))
 
-; (title (first-content route-xml))
+(route-title (route-xml))
 
-; (defn things-tagged [tag content]
-;   (map :attrs (filter #(= tag (% :tag)) content)))
+(defn things-tagged [tag content]
+  (map :attrs (filter #(= tag (% :tag)) content)))
 
-; (defn stops [xml]
-;   (things-tagged :stop ((first-content xml) :content)))
-; (count (stops (first-content route-xml)))
+(defn stops [xml]
+  (things-tagged :stop ((body xml) :content)))
+(count (stops (route-xml)))
 
 ; (defn stops-named [stop-name xml]
 ;   (filter #(= stop-name (% :title))
-;           (stops (first-content xml))))
+;           (stops (body xml))))
 ; (stops-named "City College Bookstore" route-xml)
 
 ; (defn directions [xml]
-;   (things-tagged :direction ((first-content xml) :content)))
+;   (things-tagged :direction ((body xml) :content)))
 ; (count (directions route-xml))
 
 
